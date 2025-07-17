db82e7eff38fb5382be1925ffcc225fa55bfae08
Gowtham4-4 <gowthamperumallapalli@gmail.com>
2025-07-17 22:01:45 +0530

    Update AdminDashboardPage.js

--- src/pages/AdminDashboardPage.js
+++ src/pages/AdminDashboardPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { API_BASE_URL, IMAGE_BASE_URL } from '../config';
+import API_BASE_URL from '../config';
 
 const AdminDashboardPage = () => {
   const [locations, setLocations] = useState([]);
